3bb2a71ad13b23a63b6a3fff986ee19fee06a6a6
Tony-404 <chernani2003@gmail.com>
2021-08-09 16:52:45 -0500

    Lola

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 const items = document.getElementById('items');
 const templateCard= document.getElementById('template-card').content;
 const fragment = document.createDocumentFragment();
+let like = []
+let dislike = []
 
 document.addEventListener('DOMContentLoaded', () => {
     recogerDates();
@@ -23,8 +25,51 @@ const pintarCards = data => {
         templateCard.getElementById('nombre').textContent = name;
         templateCard.querySelector('img').setAttribute('src', image);
         const clone = templateCard.cloneNode(true);
+        templateCard.querySelector('.btn-info').dataset.id = id
+        templateCard.querySelector('.btn-danger').dataset.id = id
         fragment.appendChild(clone);
     })
     items.appendChild(fragment);
 }
 
+items.addEventListener('click', e => {
+    addLikes(e)
+})
+
+const addLikes = e => {
+    if(e.target.classList.contains('btn-info')){
+        
+        setLikes(e.target.parentElement)
+    }
+    else if(e.target.classList.contains('btn-danger')){
+        setLikes2(e.target.parentElement)
+    }
+}
+
+const setLikes = objeto => {
+    const botonL = {
+        id: objeto.querySelector('.btn-info').dataset.id,
+        cantidad: 0
+    }
+    if(like.hasOwnProperty(botonL.id)){
+        botonL.cantidad = like[botonL.id].cantidad + 1;
+        objeto.querySelector('#like').textContent = botonL.cantidad
+    }
+    like[botonL.id] = {...botonL}
+    console.log(like[botonL.id])
+    
+}
+const setLikes2 = objeto2 => {
+    const botonD = {
+        id: objeto2.querySelector('.btn-danger').dataset.id,
+        cantidadD: 1
+    }
+    
+    if(dislike.hasOwnProperty(botonD.id)){
+        botonD.cantidadD = dislike[botonD.id].cantidadD + 1;
+        objeto2.querySelector('#dislike').textContent = botonD.cantidadD
+    }
+    dislike[botonD.id] = {...botonD}
+    console.log(dislike[botonD.id])
+    
+}
