fca7f41910f064a365ba7da3b176f174ec529d7c
Tony-404 <chernani2003@gmail.com>
2021-08-09 14:54:53 -0500

    index

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    recogerDates();
+})
+
+const recogerDates = async () => {
+    try{
+        const res = await fetch("../json/data.json")
+        const data = await res.json()
+        pintarCards(data)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+const pintarCards = data => {
+
+}
